05e09a435935158cd7a717924a1b800ecfb46792
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-01 21:50:19 +0530

    Initialized Multer-cloudinary setup

---
+++ File Storage System/middlewares/multerMiddleware.js
@@ -0,0 +1,28 @@
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedTypes = /jpeg|jpg|png|pdf|docx/;
+  const extname = allowedTypes.test(
+    path.extname(file.originalname).toLowerCase()
+  );
+  const mimetype = allowedTypes.test(file.mimetype);
+  if (extname && mimetype) cb(null, true);
+  else cb("Error: File type not supported");
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5,
+  },
+  fileFilter: fileFilter,
+});
+
+module.exports = upload;
