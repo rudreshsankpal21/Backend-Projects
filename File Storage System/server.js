0e5e70c7ba7ad4c694876b66aca3a8f10b609905
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-01 21:38:31 +0530

    Initialized user Auth System

--- File Storage System/server.js
+++ File Storage System/server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 // middlewares
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 
 // Connect DB
 connectDB().then(() => {
