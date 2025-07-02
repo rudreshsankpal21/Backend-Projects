f335ab5b1f7d5f35510fbbc0706d5a00cdd68a62
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-02 19:12:00 +0530

    Added URLshema

---
+++ URL shortner/models/ShortUrl.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const shortUrlSchema = new mongoose.Schema(
+  {
+    originalUrl: {
+      type: String,
+      required: true,
+    },
+    shortId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    clicks: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ShortUrl = mongoose.model("ShortUrl", shortUrlSchema);
+module.exports = ShortUrl;
