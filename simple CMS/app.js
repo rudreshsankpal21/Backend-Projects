e9dc501ea3605b67371a66a54212737ae9e9c8a5
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-03 19:37:40 +0530

    Added category routes

--- simple CMS/app.js
+++ simple CMS/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/authRoutes");
 const postRouter = require("./routes/postRoutes");
+const categoryRouter = require("./routes/categoryRoutes");
 const app = express();
 
 // middlewares
@@ -9,5 +10,6 @@ app.use(express.json());
 // routes
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
+app.use("/api/categories", categoryRouter);
 
 module.exports = app;
