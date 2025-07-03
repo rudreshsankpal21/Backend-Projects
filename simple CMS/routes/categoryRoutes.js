0a7e8baaaddc3bb5d5a0a897ebbda3aced57b760
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-03 19:35:33 +0530

    Added all routes of categoryRoutes

---
+++ simple CMS/routes/categoryRoutes.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
+const isAdmin = require("../middlewares/isAdmin");
+const categoryRouter = express.Router();
+
+// create new category
+categoryRouter.post("/create", authMiddleware, isAdmin, createCategory);
+
+// get all categories
+categoryRouter.get("/", getAllCategories);
+
+// get posts by category slug
+categoryRouter.get("/:slug", authMiddleware, getPostsByCategorySlug);
