a79ca773a1a8f7a8e21113b368a782839436a4d9
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-04 20:00:01 +0530

    Added - updatePost route logic

--- simple CMS/controllers/postController.js
+++ simple CMS/controllers/postController.js
@@ -60,4 +60,30 @@ const getPostById = async (req, res) => {
   }
 };
 
+// update Post
+const updatePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, content, category } = req.body;
+
+    // update post
+    const post = await PostModel.findByIdAndUpdate(
+      id,
+      { title, content, category },
+      { new: true }
+    );
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+    }
+
+    // check if the post belongs to the user
+    if (post.author.toString() !== req.user._id.toString()) {
+      return res.status(403).json({ message: "You are not authorized" });
+    }
+    await post.save();
+    res.status(200).json({ message: "Post updated successfully", post });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 module.exports = { createPost, getAllPosts, getPostById };
