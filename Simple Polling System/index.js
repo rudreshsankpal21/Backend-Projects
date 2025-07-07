2cde77d5df90fcd74a65916f5589eb247af5abd5
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-07 19:00:15 +0530

    Added polling routes

--- Simple Polling System/index.js
+++ Simple Polling System/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRouter = require("./routes/authRoutes");
+const pollRouter = require("./routes/pollRoutes");
 const app = express();
 
 // middlewares
@@ -7,5 +8,6 @@ app.use(express.json());
 
 // routes
 app.use("/api/auth", authRouter);
+app.use("/api/polls", pollRouter);
 
 module.exports = app;
---
+++ Simple Polling System/routes/pollRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
+const pollRouter = express.Router();
+
+// create poll
+pollRouter.post("/", authMiddleware, createPoll);
+
+// get all polls
+pollRouter.get("/", getAllPolls);
+
+// get a single poll by id
+pollRouter.get("/:id", authMiddleware, getPollById);
+
+// vote on a poll
+pollRouter.put("/:id/vote", authMiddleware, voteOnPoll);
+
+// close a poll
+pollRouter.put("/:id/close", authMiddleware, closePoll);
+
+// delete a poll
+pollRouter.delete("/:id", authMiddleware, deletePoll);
+
+module.exports = pollRouter;
