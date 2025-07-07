82b5cf5383365fccd314a84676dd9967ab41e0c2
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-07 19:05:49 +0530

    Updated Poll model

--- Simple Polling System/models/Poll.js
+++ Simple Polling System/models/Poll.js
@@ -19,9 +19,6 @@ const postSchema = new mongoose.Schema(
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
-      option: {
-        type: String,
-      },
     },
     createdBy: {
       type: mongoose.Schema.Types.ObjectId,
