b64b5c615e54ffc8ca85ad00483f7290105cbd28
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-07 18:54:21 +0530

    Added Poll model

---
+++ Simple Polling System/models/Poll.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+// Poll Model
+
+const postSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    options: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    votes: {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      option: {
+        type: String,
+      },
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    isClosed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Poll = mongoose.model("Poll", postSchema);
+module.exports = Poll;
