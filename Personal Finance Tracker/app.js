2e7b78ecb35bb97f0652aaac7f7f98145f3aa5d3
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-15 22:27:43 +0530

    feat : Add CRUD Routes for transaction

--- Personal Finance Tracker/app.js
+++ Personal Finance Tracker/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const authRouter = require("./routes/authRoutes");
+const transactionRouter = require("./routes/transactionRoutes");
 const app = express();
 
 // middlewares
@@ -7,5 +8,6 @@ app.use(express.json());
 
 // routes
 app.use("/api/auth", authRouter);
+app.use("/api/transactions", transactionRouter);
 
 module.exports = app;
