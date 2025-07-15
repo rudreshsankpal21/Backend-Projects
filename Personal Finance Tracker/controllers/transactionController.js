98e7f0964b66bfe8f2e76fdc2911b98ee61c0eed
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-15 22:28:37 +0530

    feat : Add logic for create Transaction Route

--- Personal Finance Tracker/controllers/transactionController.js
+++ Personal Finance Tracker/controllers/transactionController.js
@@ -1,7 +1,32 @@
 const Transaction = require("../models/Transaction");
 
 // Create a new transaction
-const createTransaction = async (req, res) => {};
+const createTransaction = async (req, res) => {
+  const { title, amount, type } = req.body;
+  try {
+    const transaction = await Transaction.create({
+      title,
+      amount,
+      type,
+      user: req.user._id,
+    });
+    if (!transaction) {
+      res.status(400).json({
+        message: "Failed to create Transaction",
+      });
+    }
+
+    res.status(201).json({
+      message: "Transaction created successfully",
+      transaction,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error,
+    });
+  }
+};
 
 // Get all transactions
 const getAllTransactions = async (req, res) => {};
