f05cf111e024d9614c03bc85aa6dead0ce23996b
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-15 23:27:08 +0530

    refactor: add comments

--- Personal Finance Tracker/app.js
+++ Personal Finance Tracker/app.js
@@ -12,4 +12,5 @@ app.use(express.json());
 app.use("/api/auth", authRouter);
 app.use("/api/transactions", authMiddleware, transactionRouter);
 app.use("/api/insights", authMiddleware, insightRouter);
+
 module.exports = app;
