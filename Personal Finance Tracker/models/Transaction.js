b4a9d973da17987fd2620d65dcdd53f4410ec4a7
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-15 22:14:51 +0530

    feat : Add Transaction Model

--- Personal Finance Tracker/models/Transaction.js
+++ Personal Finance Tracker/models/Transaction.js
@@ -1,3 +1,33 @@
 const mongoose = require("mongoose");
 
-// Pending.....
+// Transaction Model
+
+const transactionSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["income", "expense"],
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Transaction = mongoose.model("Transaction", transactionSchema);
+
+module.exports = Transaction;
