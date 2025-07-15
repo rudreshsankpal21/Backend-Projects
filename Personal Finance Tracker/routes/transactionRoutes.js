859f464f0a33e0a4aeb526110f8160b0320b7158
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-15 22:23:47 +0530

    refactor : add authMiddleware to CRUD operations

--- Personal Finance Tracker/routes/transactionRoutes.js
+++ Personal Finance Tracker/routes/transactionRoutes.js
@@ -6,21 +6,22 @@ const {
   updateTransaction,
   deleteTransaction,
 } = require("../controllers/transactionController");
+const authMiddleware = require("../middlewares/authMiddleware");
 const transactionRouter = express.Router();
 
 // create a new transaction
-transactionRouter.post("/create", createTransaction);
+transactionRouter.post("/create", authMiddleware, createTransaction);
 
 // get all transactions
-transactionRouter.get("/all", getAllTransactions);
+transactionRouter.get("/all", authMiddleware, getAllTransactions);
 
 // get all transactions of a user
-transactionRouter.get("/user/:id", getTransactionById);
+transactionRouter.get("/user/:id", authMiddleware, getTransactionById);
 
 // update a transaction
-transactionRouter.put("/update/:id", updateTransaction);
+transactionRouter.put("/update/:id", authMiddleware, updateTransaction);
 
 // delete a transaction
-transactionRouter.delete("/delete/:id", deleteTransaction);
+transactionRouter.delete("/delete/:id", authMiddleware, deleteTransaction);
 
 module.exports = transactionRouter;
