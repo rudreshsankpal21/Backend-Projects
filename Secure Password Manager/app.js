42729ebbcd4b042d460af74b853b716c6c06b770
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-16 22:30:47 +0530

    feat : add User Authentication

--- Secure Password Manager/app.js
+++ Secure Password Manager/app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const authRouter = require("./routes/authRoutes");
+const authMiddleware = require("./middlewares/authMiddleware");
 const app = express();
 
 // middlewares
 app.use(express.json());
 
 // routes
+app.use("/api/auth", authRouter);
 
 module.exports = app;
---
+++ Secure Password Manager/routes/authRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const authRouter = express.Router();
+const {
+  registerUser,
+  loginUser,
+  getUsers,
+} = require("../controllers/authController");
+authRouter.post("/register", registerUser);
+
+authRouter.post("/login", loginUser);
+
+authRouter.get("/users", getUsers);
+
+module.exports = authRouter;
