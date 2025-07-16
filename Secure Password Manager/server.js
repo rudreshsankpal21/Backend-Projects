17875eb1cd4ff794f3fa32d2f2c0d7f503130848
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-16 22:42:27 +0530

    add DataBase connection with server

--- Secure Password Manager/server.js
+++ Secure Password Manager/server.js
@@ -1,6 +1,12 @@
+require("dotenv").config();
 const app = require("./app");
+const connectDB = require("./config/db");
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () =>
-  console.log(`Server is running on http://localhost:${PORT}`)
-);
+// connect DB
+connectDB().then(() => {
+  // start server
+  app.listen(PORT, () =>
+    console.log(`Server is running on http://localhost:${PORT}`)
+  );
+});
