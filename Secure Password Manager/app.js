e3c9f68d8222690a39977e707541fa945446baf8
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-17 19:08:42 +0530

    feat : add CRUD routes for password

--- Secure Password Manager/app.js
+++ Secure Password Manager/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/authRoutes");
 const authMiddleware = require("./middlewares/authMiddleware");
+const passwordRouter = require("./routes/passwordRoutes");
 const app = express();
 
 // middlewares
@@ -8,5 +9,6 @@ app.use(express.json());
 
 // routes
 app.use("/api/auth", authRouter);
+app.use("api/passwords", authMiddleware, passwordRouter);
 
 module.exports = app;
---
+++ Secure Password Manager/routes/passwordRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const passwordRouter = express.Router();
+
+// get all passwords of user
+passwordRouter.get("/", authMieware, getAllPasswords);
+
+// get one password by ID
+passwordRouter.get("/:id", getPasswordById);
+
+// Add a new Password
+passwordRouter.post("/", addPassword);
+
+// Update a Password
+passwordRouter.put("/:id", updatePassword);
+
+// Delete a Password
+passwordRouter.delete("/:id", deletePassword);
+
+module.exports = passwordRouter;
