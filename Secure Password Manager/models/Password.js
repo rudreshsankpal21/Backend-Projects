9725353ac1caa4d5b110bdf42bad7aa554232850
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-17 19:03:38 +0530

    feat : Add Password Model

---
+++ Secure Password Manager/models/Password.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const passwordSchema = new mongoose.Schema(
+  {
+    User: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    serviceName: {
+      type: String,
+      required: true,
+    },
+    emailOrUsername: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    notes: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Password = mongoose.model("Password", passwordSchema);
+
+module.exports = Password;
