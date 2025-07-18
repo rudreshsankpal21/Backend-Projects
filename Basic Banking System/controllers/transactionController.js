081e818193deca51b74f2c2e1b460a8d11a694d1
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-18 19:34:22 +0530

    feat : add logic for withdraw the amount

--- Basic Banking System/controllers/transactionController.js
+++ Basic Banking System/controllers/transactionController.js
@@ -31,7 +31,49 @@ const deposite = async (req, res) => {
 };
 
 // withdraw balance
-const withdraw = async (req, res) => {};
+const withdraw = async (req, res) => {
+  try {
+    // Track the balance
+    const user = await User.findById(req.user._id).select("balance");
+
+    if (!user) {
+      return res.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    // no withdrawal allowed if balance is 0
+    if (user.balance === 0) {
+      return res.status(400).json({
+        message: "No withdrawal allowed as your balance is 0",
+      });
+    }
+
+    // withdraw balance
+    const transaction = await Transaction.create({
+      user: req.user._id,
+      toUser: req.user._id,
+      type: "withdrawal",
+      amount,
+    });
+
+    if (!transaction) {
+      res.status(400).json({
+        message: "Failed withdrawing the amount",
+      });
+    }
+
+    res.status(200).json({
+      message: "successfully withdrawed the amount",
+      transaction,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error",
+      error,
+    });
+  }
+};
 
 // transfer balance
 const transfer = async (req, res) => {};
--- Basic Banking System/routes/transactionRoutes.js
+++ Basic Banking System/routes/transactionRoutes.js
@@ -12,7 +12,7 @@ const {
 transactionRouter.post("/deposite", authMiddleware, deposite); // ✅
 
 // withdraw Balance
-transactionRouter.post("/withdraw", authMiddleware, withdraw);
+transactionRouter.post("/withdraw", authMiddleware, withdraw); // ✅
 
 // transfer Balance
 transactionRouter.post("/transfer", authMiddleware, transfer);
