b4138f160af17dc8f022ad1e912ef8198adbb1ee
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-18 19:23:36 +0530

    feat : add routes Core banking features

---
+++ Basic Banking System/controllers/transactionController.js
@@ -0,0 +1,16 @@
+const Transaction = require("../models/Transaction");
+
+// deposite balance
+const deposite = async (req, res) => {};
+
+// withdraw balance
+const withdraw = async (req, res) => {};
+
+// transfer balance
+const transfer = async (req, res) => {};
+
+module.exports = {
+  deposite,
+  withdraw,
+  transfer,
+};
---
+++ Basic Banking System/routes/transactionRoutes.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const transactionRouter = express.Router();
+
+const authMiddleware = require("../middlewares/authMiddleware");
+const {
+  deposite,
+  withdraw,
+  transfer,
+} = require("../controllers/transactionController");
+
+//  deposite Balance
+transactionRouter.post("/deposite", authMiddleware, deposite);
+
+// withdraw Balance
+transactionRouter.post("/withdraw", authMiddleware, withdraw);
+
+// transfer Balance
+transactionRouter.post("/transfer", authMiddleware, transfer);
+
+module.exports = transactionRouter;
