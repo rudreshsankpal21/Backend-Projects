b0f3b33f9ef5fb7b4e6e4cae6d1ffc90e21f7ba4
rudresh21 <rudreshsankpal21@gmail.com>
2025-07-18 19:35:40 +0530

    feat : add routes Core banking features

--- Basic Banking System/app.js
+++ Basic Banking System/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const authRouter = require("./routes/authRoutes");
 const authMiddleware = require("./middlewares/authMiddleware");
+const transactionRouter = require("./routes/transactionRoutes");
 const app = express();
 
 // middlewares
@@ -9,5 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.use("/api/auth", authMiddleware, authRouter);
+app.use("/api/account", authMiddleware, transactionRouter);
 
 module.exports = app;
